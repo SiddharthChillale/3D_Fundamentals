7ccacbc76b9abc093c5f4e80accf4c9a71709b92
siddharth <siddharth.chillale@gmail.com>
2022-07-04 20:17:38 -0400

    add pixel shader to the pipeline

--- Engine/Pipeline.h
+++ Engine/Pipeline.h
@@ -10,73 +10,11 @@
 
 // fixed-function triangle drawing pipeline
 // draws textured triangle lists with clamping
+template<class Effect>
 class Pipeline {
 public:
 	// vertex type used for geometry and throughout pipeline
-	class Vertex
-	{
-	public:
-		Vertex() = default;
-		Vertex(const Vec3& pos)
-			:
-			pos(pos)
-		{}
-		// this enables template functions clone a vertex
-		// while changing the pos only
-		Vertex(const Vec3& pos, const Vertex& src)
-			:
-			t(src.t),
-			pos(pos)
-		{}
-		Vertex(const Vec3& pos, const Vec2& t)
-			:
-			t(t),
-			pos(pos)
-		{}
-		Vertex& operator+=(const Vertex& rhs)
-		{
-			pos += rhs.pos;
-			t += rhs.t;
-			return *this;
-		}
-		Vertex operator+(const Vertex& rhs) const
-		{
-			return Vertex(*this) += rhs;
-		}
-		Vertex& operator-=(const Vertex& rhs)
-		{
-			pos -= rhs.pos;
-			t -= rhs.t;
-			return *this;
-		}
-		Vertex operator-(const Vertex& rhs) const
-		{
-			return Vertex(*this) -= rhs;
-		}
-		Vertex& operator*=(float rhs)
-		{
-			pos *= rhs;
-			t *= rhs;
-			return *this;
-		}
-		Vertex operator*(float rhs) const
-		{
-			return Vertex(*this) *= rhs;
-		}
-		Vertex& operator/=(float rhs)
-		{
-			pos /= rhs;
-			t /= rhs;
-			return *this;
-		}
-		Vertex operator/(float rhs) const
-		{
-			return Vertex(*this) /= rhs;
-		}
-	public:
-		Vec3 pos;
-		Vec2 t;
-	};
+	typedef typename Effect::Vertex Vertex;
 
 public:
 	Pipeline(Graphics& gfx) 
@@ -92,9 +30,7 @@ public:
 	void BindTranslation(const Vec3& translation_in) {
 		translation = translation_in;
 	}
-	void BindTexture(const std::wstring& filename) {
-		pTex = std::make_unique<Surface>(Surface::FromFile(filename));
-	}
+	
 
 private:
 
@@ -254,12 +190,7 @@ private:
 		itEdge0 += dv0 * (float(yStart) + 0.5f - it0.pos.y);
 		itEdge1 += dv1 * (float(yStart) + 0.5f - it0.pos.y);
 
-		// prepare clamping constants
-		const float tex_width = float(pTex->GetWidth());
-		const float tex_height = float(pTex->GetHeight());
-		const float tex_xclamp = tex_width - 1.0f;
-		const float tex_yclamp = tex_height - 1.0f;
-
+		
 		for (int y = yStart; y < yEnd; y++, itEdge0 += dv0, itEdge1 += dv1)
 		{
 			// calculate start and end pixels
@@ -281,14 +212,12 @@ private:
 			for (int x = xStart; x < xEnd; x++, iLine += diLine)
 			{
 				// perform texture lookup, clamp, and write pixel
-				gfx.PutPixel(x, y, pTex->GetPixel(
-					(unsigned int)std::min(iLine.t.x * tex_width + 0.5f, tex_xclamp),
-					(unsigned int)std::min(iLine.t.y * tex_height + 0.5f, tex_yclamp)
-				));
+				gfx.PutPixel(x, y, effect.ps(iLine));
 			}
 		}
 	}
-
+	public:
+		Effect effect;
 private:
 	Graphics& gfx;
 	Mat3 rotation;
